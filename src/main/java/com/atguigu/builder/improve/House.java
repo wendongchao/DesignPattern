58422ea335c9ffbb53eda855a2187c54fd20000f
wendongchao <dongchao1860369@sina.com>
2022-08-16 21:58:03 +0800

    设计模式

---
+++ src/main/java/com/atguigu/builder/improve/House.java
@@ -0,0 +1,12 @@
+package com.atguigu.builder.improve;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class House {
+    private String baise;
+    private String wall;
+    private String roofed;
+}
